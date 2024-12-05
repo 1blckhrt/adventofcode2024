5f836d8fb42ac4b21cb3dd79c16c39ed6be995ea
1blckhrt <williams.1691@wright.edu>
2024-12-04 20:40:24 -0500

    feat: day1 complete

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,23 @@
-# 
+left = []
+right = []
+total = 0
+total2 = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        left_value, right_value = map(int, line.split())
+        left.append(left_value)
+        right.append(right_value)
+
+sorted_left = sorted(left)
+sorted_right = sorted(right)
+
+for l, r in zip(sorted_left, sorted_right):
+    total += abs(l - r)
+
+print(total)
+
+for l in sorted_left:
+    total2 += l * sorted_right.count(l)
+    
+print(total2)
